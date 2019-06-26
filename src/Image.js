903a7864505cc7805f4203b01ad2baeaebc75c87
Katelyn Truong <A00952509@ad.bcit.ca>
2019-06-25 20:19:44 -0700

    cz

---
+++ src/Image.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import cat from './cat.jpg';
+
+function Image(){
+    return (
+        <img  src={cat} alt="my images"/>
+    );
+}
+
+export default Image;
--- src/Profile.js
+++ src/Profile.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Image from './Image';
 function Profile(){
     return(
-        <div>Profile test</div>
+        <div>
+            <h1>Profile test</h1>
+            <p>This is a description</p>
+            <Image />
+        </div>
+      
     );
 }
 
